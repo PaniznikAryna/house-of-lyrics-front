b01479490039e3901fa5e1c738dd2c30268d82d4
start.vaadin.com <no-reply@vaadin.com>
2025-04-24 19:41:40 +0000

    Generated project

---
+++ src/main/java/net/houseoflyrics/base/domain/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable domain classes.
+ */
+@NullMarked
+package net.houseoflyrics.base.domain;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/net/houseoflyrics/base/ui/component/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable UI components.
+ */
+@NullMarked
+package net.houseoflyrics.base.ui.component;
+
+import org.jspecify.annotations.NullMarked;
